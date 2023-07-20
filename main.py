0188741164e8f4b99d11ce2a1016fde2a82a8ad6
Leticia <124852398+LeticiaBHB@users.noreply.github.com>
2023-07-20 20:15:48 -0300

    sixth commit main.py
    added float and int values ​​to average and sum results by country

--- main.py
+++ main.py
@@ -29,33 +29,45 @@ for col in colunas_existentes:
 # Substituir valores NaN (causados pelos traços) por 0 para calcular a média
 df = df.fillna(0)
 
-# Configurar a exibição dos valores float formatados
-pd.set_option('display.float_format', '{:.15g}'.format)
-
 # Calcular a média para cada coluna de ano (de 1990 a 2019) e armazenar em media_por_ano
 media_por_ano = df[colunas_existentes].mean()
-print(media_por_ano)
 
 # Calcular a média apenas para as colunas numéricas
 df['Média'] = df[colunas_existentes].mean(axis=1)
 
-# Configurar a exibição dos valores float formatados
-pd.set_option('display.float_format', '{:.15f}'.format)
-
 # Calcular a soma total de cada linha (para as colunas de 1990 a 2020)
 df['Soma_Total'] = df[colunas_existentes].sum(axis=1)
 
-# Exibir todos os nomes da tabela Country junto com a média calculada para cada linha
-result = df[['Country', 'Soma_Total', 'Média']]
+# Remover o caractere '-' das colunas 'Soma_Total' e 'Média'
+df['Soma_Total'] = df['Soma_Total'].astype(str).str.replace('-', '')
+df['Média'] = df['Média'].astype(str).str.replace('-', '')
+
+# Criar colunas extras com os valores inteiros das somas e médias, removendo o '.' dos valores float antes de converter para int
+df['ValorRealSomaTotal'] = df['Soma_Total'].astype(float).astype(int)
+df['ValorRealMedia'] = df['Média'].astype(float).abs().astype(int)
+
+# Exibir todos os nomes da tabela Country junto com as somas e médias calculadas para cada linha
+result = df[['Country', 'Soma_Total', 'ValorRealSomaTotal', 'Média', 'ValorRealMedia']]
+
+# Configurar a exibição dos valores float formatados com todas as casas decimais
+pd.options.display.float_format = '{:,.15f}'.format
 
 print(result)
-
-# Calcular a soma completa dos valores da tabela CSV de 1990 até 2020
-soma_completa = df[colunas_existentes].sum().sum()
 
 # Salvar o dataframe result em um arquivo CSV
 result.to_csv('resultado.csv', index=False)
 
-# Criar um dataframe com a soma completa e salvar em um arquivo CSV
+# Calcular a soma completa dos valores da tabela CSV de 1990 até 2020
+soma_completa = df[colunas_existentes].sum().sum()
+
+# Criar um dataframe com a soma completa e a média por ano e salvar em um arquivo CSV
 soma_df = pd.DataFrame({'Soma_Completa': [soma_completa]})
-soma_df.to_csv('soma_completa.csv', index=False, float_format='%.15f')
+soma_df = soma_df.assign(**media_por_ano)
+
+# Configurar a exibição dos valores float formatados com todas as casas decimais para a soma completa
+pd.options.display.float_format = '{:,.15f}'.format
+
+print(soma_df)
+
+# Salvar o dataframe soma_df em um arquivo CSV
+soma_df.to_csv('soma_completa.csv', index=False)
